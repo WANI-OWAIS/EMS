9ff3c1ca0bd35416d0e98538d527dc2d46b10b76
WANI-OWAIS <waniowais528@gmail.com>
2025-03-06 23:01:10 +0530

    somefilesupdated

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 function App() {
+  const [user, setUser] = useState(null)
+  const handleLogin = (email,password) =>{
+      if(email == 'admin@me.com' && password == '123'){
+        setUser('admin')
+        console.log(user)
+  }else if(email == 'user@me.com' && password == '123'){
+        setUser('employee')
+        console.log(user)
+  }
+  else{
+    alert('Invalid Credentials')
+  }
+}
+
   return (
     <>
-    {/* <Login /> */}
-    {/* <EmployeeDashboard /> */}
-    <AdminDashboard />
+   {!user ? <Login handleLogin={handleLogin} /> : ''}
+   {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
 }
